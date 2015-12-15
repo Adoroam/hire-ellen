832536ae3c1422910c45362a7f64e988ea6f84ae
Anthony Stabile <Anthony Stabile>
2015-12-15 17:50:22 -0600

    added nganimate module, put modal lists in angular and added class validation, updated php email message, added animation to front page content, removed jquery, changed fonts, changed some colors, rewrote validation scripts, converted most of everything to angularjs.

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
-<html>
+<html ng-app="ang">
 <head>
 	<title>Hire Ellen</title>
-	<link href='http://fonts.googleapis.com/css?family=Cardo' rel='stylesheet' type='text/css'>
-	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js"></script>
+	<link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Libre+Baskerville' rel='stylesheet' type='text/css'>
+	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.0/angular.min.js"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.4.0/angular-animate.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script src="libs/zero.js"></script>	
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
@@ -12,97 +14,108 @@
 	<link rel="stylesheet" href="libs/style.css">
 	<meta name="Website Author" content="Anthony Stabile">
 </head>
-<body ng-app="ang" ng-controller="MainController">
-	<div id="alertBox"> <?php
-		if (isset($_POST['clientEmail'])) {
-		$clientEmail = $_POST['clientEmail'];
-		$clientName = $_POST['clientName'];
-		if ($clientName == "") { 
-		   $clientName = "No Name Provided";
-		}
-		$msg = "You have a new business inquiry from " . $clientName . "\nTheir email address is: " . $clientEmail;
-		mail("llngrmn@gmail.com","new hire-ellen.com client",$msg);
-		}
+<body ng-controller="MainController">
+	<div id="alertBox"> 
+<?php
+	if (isset($_POST['clientEmail'])) {
+	$clientName = $_POST['clientName'];
+	$clientEmail = $_POST['clientEmail'];
+	$clientProject = $_POST['clientProject'];
+	$clientStage = $_POST['clientStage'];
+	$clientDeadline = $_POST['clientDeadline'];
+
+
+	$msg = "You have a new business inquiry from " . $clientName 
+		. "\nTheir email address is: " . $clientEmail
+		."\nThe project type is: " . $clientProject
+		."\nThe stage is: " . $clientStage
+		."\nThe deadline is: " . $clientDeadline;
+
+	mail("llngrmn@gmail.com","new hire-ellen.com client",$msg);
+	}
 ?></div>
 	<div class="container-fluid scooch">
 		<div class="row beige ">
+			<div class="row top-spacer"></div>
+<!-- CONTENT -->
 			<div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1">
 				<div class="row content">
 					<div id="afix2" class="col-lg-6 col-lg-push-6 col-md-6 col-md-push-6 col-sm-6 col-sm-push-6 col-xs-12">				
-						<h3>Hello. I'm Ellen. I make beautiful content.</h3>
+						<h3>Hello. I'm Ellen. I make books happen.</h3>
+
+						<div ng-repeat="x in cList">
+							<div class="list-group">
+								<button type="button" class="list-group-item" ng-click="setActive($index);">{{ x.title }}<span class="caret pull-right"></span></button>
+								<div ng-include="x.page" class="list-group-item well dropContent"  ng-if="active == x">
+									
+								</div>
+							</div>
+						</div>
+<!-- MODAL BUTTON -->
 						<div type="button" class="btn btn-default modalButton" data-toggle="modal" data-target="#sliderModal">
                         				Let's Talk
                     				</div>
 					</div>	<!-- end afix2 -->
-					<div id="afix1" class="col-lg-6 col-lg-pull-6 col-md-6 col-md-pull-6 col-sm-6 col-sm-pull-6 col-xs-12 ">		
+<!-- IMAGE -->
+					<div id="afix1" class="col-lg-6 col-lg-pull-6 col-md-6 col-md-pull-6 col-sm-6 col-sm-pull-6 col-xs-12 ">
 						<img  title="Office vector designed by Freepik" class="img-responsive" src="http://i.imgur.com/nKWQnh7.png" />
 					</div>
-					
+<!-- END IMAGE -->					
 				</div> <!--end internal row-->
 			</div>		
 		</div>
 	</div>
 <!-- MODAL -->               
-	<div class="modal fade" id="sliderModal" tabindex="-1" role="dialog" aria-labelledby="sliderModal">
-	  <div class="modal-dialog" role="document">
-	    <div class="modal-content">
-	      <div class="modal-header">
-	        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-	        <h4 class="modal-title center" id="sliderModal">Let's Talk</h4>
-	      </div>
-	      <div class="modal-body">
-	      	<p class="center">I need five small pieces of information to get started.</p>
-			<form id="wholeForm" class="" action="" method="POST">
-				<div id="formEmail" class="form-group has-feedback">
-				  	<label class="control-label sr-only" for="clientEmail">Input group with success</label>
-					<div class="input-group">
-						<div class="input-group-addon">Name:</div>
-						<input type="text" class="form-control" id="clientName" name="clientName" aria-describedby="inputGroupSuccess3Status" placeholder="What should I call you?"><!-- name input -->
+<div class="modal fade" id="sliderModal" tabindex="-1" role="dialog" aria-labelledby="sliderModal">
+	<div class="modal-dialog" role="document">
+    		<div class="modal-content">
+      			<div class="modal-header">
+        			<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        			<h4 class="modal-title center" id="sliderModal">Let's Talk</h4>
+      			</div>
+      			<div class="modal-body">
+      				<p class="center">I need five small pieces of information to get started.</p>
+<!-- FORM -->
+				<form id="wholeForm" class="" action="" method="POST">
+
+				<div ng-repeat="x in lists | filter: 'input' " class="form-group {{x.valid}}" id="{{ x.id }}">
+					<label class="control-label sr-only" for="{{x.name}}">Input group with success</label>
+						<div class="input-group">
+							<div class="input-group-addon">{{x.obj}}</div>
+							<input type="text" class="form-control" id="{{x.name}}"  name="{{x.name}}" 
+							aria-describedby="{{x.desc}}" placeholder="{{x.placeholder}}" ng-model="x.sel" ng-change="validate(x);">
+							<span id="{{x.desc}}" class="sr-only">(success)</span>
+						</div>
+					</div> 
+
+					<div ng-repeat="x in lists | filter: 'dropdown' " class="form-group {{x.valid}}" id="{{ x.id }}">
+						<label class="control-label sr-only" for="{{x.name}}">Input group with success</label>
+						<div class="input-group">					
+							<div class="input-group-addon">{{x.obj}}</div>
+								<select class="form-control" name="{{x.name}}" 
+								aria-describedby="{{x.desc}}" ng-model="x.sel" ng-change="validate(x);">
+									<option selected hidden>{{x.sel}}</option>
+									<option ng-repeat="y in x.option" >{{y}}</option>					
+								</select>
+								<span id="{{x.desc}}" class="sr-only">(success)</span>
+						  </div>					  
 					</div>
-					<div class="input-group">
-						<div class="input-group-addon">Email:</div>
-						<input type="text" class="form-control" id="clientEmail" name="clientEmail" aria-describedby="clientEmailStatus" placeholder="Where can I talk to you?"><!-- email input -->
-				    		<span id="clientEmailStatus" class="sr-only">(success)</span>				    	
-					</div>
-					<div class="input-group">
-						<div class="input-group-addon">Project:</div>
-						<input type="text" class="form-control" name="clientProject" aria-describedby="clientEmailStatus" placeholder="Fiction/Non-Fiction | Ghostwriting/Editing"><!-- project input -->
-					</div>
-					<div class="input-group">
-						<div class="input-group-addon">Stage:</div>
-						<select class="form-control" name="clientStage">
-							<option disabled selected hidden>Where is the project at?</option>
-							<option>I haven't got an idea yet, just a general feeling.</option>
-							<option>I've got a pretty good idea of what I want, but haven't written anything.</option>
-							<option>I've written a small bit and given up, or I'm overwhelmed and need direction.</option>
-							<option>I've written most or all of it, but it still needs major work to get it where I want it to be</option>
-							<option>I've written all of it and I'm looking for a second opinion or editor.</option>
-						</select>
-					</div>
-					<div class="input-group">
-						<div class="input-group-addon">Deadline:</div>
-						<select class="form-control" name="clientStage">
-							<option disabled selected hidden>How soon do you need this project done?</option>
-							<option>I don't really have any set deadline in mind.</option>
-							<option>I'd like this done in a couple of months.</option>
-							<option>I need this done in a month.</option>
-							<option>I need this done in two weeks.</option>
-							<option>I need this done yesterday.</option>
-						</select>
-					</div>
-				</div>	
-				<div class="insideBox">
-					<p class="small center">When I receive this information I will reach out within 48 hours. I will send you a simple hand written email with a few questions about your project.</p>	
-					<p class="small center">I promise never to spam you, annoy you, copy/paste generic responses, or send you silly chain emails.</p>	
-					<p class="small center ital">Note: I cannot currently take on projects where I must begin writing/editing before Jan 16th. I am booked solid. All spots after Jan 16th are still open if you wish to grab yours in advance.</p>							
-				</div>
-	  			<button id="scoot" class="btn btn-default form-control" type="submit">Submit</button>
-			</form>	
-	      </div>
-	    </div>
-	  </div>
+
+					<div class="insideBox">
+						<p class="small center">When I receive this information I will reach out within 48 hours. I will send you a simple hand written email with a few questions about your project.</p>	
+						<p class="small center">I promise never to spam you, annoy you, copy/paste generic responses, or send you silly chain emails.</p>	
+						<p class="small center ital">Note: I cannot currently take on projects where I must begin writing/editing before Jan 16th. I am booked solid. All spots after Jan 16th are still open if you wish to grab yours in advance.</p>							
+					</div>	
+					<div class="center">
+						<button id="scoot" class="btn btn-default {{submitStatus()}}" type="submit">Submit</button>		
+									
+					</div>			
+
+				</form>	
+      			</div>
+    		</div>
 	</div>
+</div>
 <!-- END MODAL -->
-				<!-- <p class="small center">*I am comfortable writing about any non-adult subject. Some subjects take longer to write about than others. I provide exact timelines with my project quotes.</p> -->
 </body>
 </html>
--- libs/zero.js
+++ libs/zero.js
@@ -1,62 +1,136 @@
 //angular
 
-var app = angular.module("ang", []);
+var app = angular.module("ang", ['ngAnimate']);
 
 app.controller('MainController', ['$scope', function($scope) {
-    
-//slider change save to standby and set preview
-    $scope.rgbInternal = function(r, g, b, index) {
-           
-    }
+//list of content pages
+	$scope.cList = [
+		{title: "What do you do?", id: "one", page: "pages/what.html"},
+		{title: "What are your prices?", id: "two", page: "pages/prices.html"},
+		{title: "Why should I choose you?", id: "three", page: "pages/why.html"}
+	];
+	$scope.active = $scope.cList[0];
+//set active content item
+	$scope.setActive = function(index) {
+    		if ($scope.active == $scope.cList[index]) {
+    			$scope.active = false;
+    		}	else { $scope.active = $scope.cList[index]; }
+    	};
+//dropdown lists
+    	$scope.lists = [
+    		{	
+    			type: "input",
+    			obj: "Name",
+    			id: "formName",
+    			name: "clientName",
+    			desc: "clientNameStatus",
+    			sel:  "",
+    			placeholder: "What should I call you?",
+    			valid: "has-feedback"
+    		},
+    		{	
+    			type: "input",
+    			obj: "Email",
+    			id: "formEmail",
+    			name: "clientEmail",
+    			desc: "clientEmailStatus",
+    			sel:  "",
+    			placeholder: "Where can I talk to you?",
+    			valid: "has-feedback"
+    		},
+    		{	
+    			type: "dropdown",
+    			obj: "Project",
+    			id: "formProject",
+    			name: "clientProject",
+    			desc: "clientProjectStatus",
+    			sel:  "Fiction/Non-Fiction | Ghostwriting/Editing",
+    			option: [
+    				"Fiction Ghostwriting",
+    				"Non-Fiction Ghostwriting",
+    				"Fiction Editing",
+    				"Non-Fiction Editing"
+    			],
+    			valid: "has-feedback"
+    		},
+    		{	
+    			type: "dropdown",
+    			obj: "Stage",
+    			id: "formStage",
+    			name: "clientStage",
+    			desc: "clientStageStatus",
+    			sel:  "How much progress have you made?",
+    			option: [
+    				"I haven't got an idea yet, just a general feeling.",
+    				"I've got a pretty good idea of what I want, but haven't written anything.",
+    				"I've written a small bit and given up, or I'm overwhelmed and need direction.",
+    				"I've written most or all of it, but it still needs major work to get it where I want it to be.",
+    				"I've written all of it and I'm looking for a second opinion or editor."
+    			],
+    			valid: "has-feedback"
+    		},
+    		{	
+    			type: "dropdown",
+    			obj: "Deadline",
+    			id: "formDeadline",
+    			name: "clientDeadline",
+    			desc: "clientDeadlineStatus",
+    			sel:  "How soon do you need this project done?",
+    			option: [
+    				"I don't really have any set deadline in mind.",
+    				"I'd like this done in a couple of months.",
+    				"I need this done in a month.",
+    				"I need this done in two weeks.",
+    				"I need this done yesterday."
+    			],
+    			valid: "has-feedback"
+    		}
+    	];
+	
+	function validateEmail(email) {
+	    var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+	    return re.test(email);
+	}
+	$scope.validate = function(x){	
+		if (x.obj == "Email") {
+			if (validateEmail(x.sel)){
+				x.valid = "has-success";
+			}	else if (x.sel == "") {
+				 x.valid = "has-feedback";
+			}	else {x.valid = "has-error";}
+			
+		}
+		if (x.obj == "Name") {
+			if (x.sel == "") {
+				x.valid = "has-feedback";
+			}	else if (x.sel.length < 3) {
+				x.valid = "has-error";
+			}	else if (x.sel.length >= 50) {
+				x.valid = "has-error";
+			}	else {x.valid = "has-success";}
+		}
+		if (x.type == 'dropdown') {
+			for (y in x.option) {
+				if (x.sel == x.option[y]) {
+					x.valid = "has-success";
+				}	
+			}			
+		}
+   	}
+   	$scope.submitStatus = function() {
+   		var status = [];
+   		for (x in $scope.lists) {
+   			status[x] = $scope.lists[x].valid;
+   		}
+   		var key = 0;
+   		for (x in status) {
+   			if (status[x] == "has-success") {
+   				key += 1;
+   			}
+   		}
+   		if (key == 5) {
+   			return "";
+   		}	else {return "disabled";}
+   	} 
    
 }]);
-
-
-//javascript functions/variables
-function validateEmail(email) {
-    var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
-    return re.test(email);
-}
-function align(outside, inside){
-	var bfix1 = $(outside).height();	 
-	var bfix2 = $(inside).height();
-	var bmargin = (bfix1 - bfix2) / 2 -5;
-	if (bmargin <= 0) { 
-		bmargin = 0; 
-	}	
-	$(inside).css("margin-top", bmargin );
-	$(inside).css("margin-bottom", bmargin);
-}
-function formColor () {
-	var emailText = $("#clientEmail").val();
-	if (validateEmail(emailText) == true) {
-		$("#formEmail").removeClass("has-error");
-		$("#formEmail").addClass("has-success");			
-	} else if  (emailText == "")  {
-		$("#formEmail").removeClass("has-error");
-		$("#formEmail").removeClass("has-success");			
-	} else {
-		$("#formEmail").addClass("has-error");
-	};
-	var emailText = $("#clientEmail").val();
-	if (validateEmail(emailText) == true) {
-		$('#scoot').prop("disabled", false);
-	}	else {
-		$('#scoot').prop("disabled", true);
-	};			
-}
-//jquery
-$(document).ready(function(){	
-	align($("#afix1"), $("#afix2"));
-	align($(window), $(".scooch"));
-	$(window).resize(function(){
-		align($("#afix1"), $("#afix2"));
-		align($(window), $(".scooch"));
-	});
-	$('#clientEmail').keyup(function( event ) { 
-		formColor(); 
-	});
-	$('#clientEmail').change(function( event ) { 
-		formColor(); 
-	});
-});
